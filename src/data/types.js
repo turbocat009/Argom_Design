3caf1242b81fd8262a9b4656a58ff0d695844380
turbocat009 <76442361+turbocat009@users.noreply.github.com>
2025-03-22 17:18:39 +0100

    Added Content

---
+++ src/data/types.js
@@ -0,0 +1,146 @@
+export const types = [
+    {
+        id: 'rascadores',
+        name: 'RASCADORES',
+        subtypes: [
+            {
+                name: 'CAJERA CERRADA',
+                content: [
+                    {
+                        name: 'R02',
+                        description: 'Rascador de dos alturas para cajera cerrada en poliuretano',
+                    },
+                    {
+                        name: 'R03',
+                        description: 'Rascador tres alturas para cajera cerrada en poliuretano',
+                    },
+                    {
+                        name: 'CR2',
+                        description: 'Collarín - rascador de dos alturas para cajera cerrada en poliuretano',
+                    },
+                    {
+                        name: 'CR3',
+                        description: 'Collarín - rascador de tres alturas para alojamiento cerrado en NBR',
+                    },
+                    {
+                        name: 'PP',
+                        description: 'Junta de amortiguación de final de carrera',
+                    },
+                    {
+                        name: 'BWA',
+                        description: 'Collarín rascador de dos alturas para neumática',
+                    },
+                    {
+                        name: 'EL',
+                        description: 'Collarín - rascador neumático de tres alturas para alojamiento cerrado',
+                    },
+                    {
+                        name: 'EM',
+                        description: 'Collarín - rascador neumático de dos alturas para alojamiento cerrado',
+                    },
+                    {
+                        name: 'EF',
+                        description: 'Collarín - rascador de tres alturas para cilindros neumático anti - rotación',
+                    },
+                    {
+                        name: 'V6',
+                        description: 'Junta de amortiguación de final de carrera',
+                    },
+                    {
+                        name: 'AY',
+                        description: 'Collarín - rascador de dos alturas con alojamiento cerrado',
+                    },
+                    {
+                        name: 'R02I',
+                        description: 'Collarín - rascador de dos alturas con alojamiento cerrado',
+                    },
+                    {
+                        name: 'CSW',
+                        description: 'Rascador de poliuretano para alojamiento cerrado con labio posterior de apoyo',
+                    },
+                    {
+                        name: 'WRS',
+                        description: 'Rascador PA cajera cerrada',
+                    },
+                    {
+                        name: 'CR24',
+                        description: 'Rascador de dos alturas con cajera inclinada y cerrada',
+                    },
+                    {
+                        name: 'H',
+                        description: 'Collarin - rascador para alojamiento cerrado',
+                    },
+                ]
+            },
+            {
+                name: 'CAJERA ABIERTA',
+            },
+            {
+                name: 'CIERRE POR SEGUIR'
+            },
+        ],
+        searchValues: [
+            {
+                name: 'Interior',
+            },
+            {
+                name: 'Exterior',
+            },
+            {
+                name: 'Altura',
+            }
+        ]
+    },
+    {
+        id: 'juntas-de-vastago',
+        name: 'JUNTAS DE VÁSTAGO',
+    },
+    {
+        id: 'juntas-de-piston',
+        name: 'JUNTAS DE PISTÓN',
+    },
+    {
+        id: 'elementos-de-guiado',
+        name: 'ELEMENTOS DE GUIADO',
+    },
+    {
+        id: 'toricas-y-aros-de-apoyo',
+        name: 'TÓRICAS Y AROS DE APOYO',
+    },
+    {
+        id: 'retenes',
+        name: 'RETENES',
+    },
+    {
+        id: 'rotativas',
+        name: 'ROTATIVAS',
+    },
+    {
+        id: 'juntas-para-racord-y-brida',
+        name: 'JUNTAS PARA RACORD Y BRIDA',
+    },
+    {
+        id: 'cajas',
+        name: 'CAJAS',
+    },
+    {
+        id: 'rotulas',
+        name: 'RÓTULAS',
+    },
+    {
+        id: 'fuelles',
+        name: 'FUELLES',
+    },
+    {
+        id: 'perfiles-y-planchas',
+        name: 'PERFILES Y PLANCHAS',
+    },
+    {
+        id: 'herramientas',
+        name: 'HERRAMIENTAS',
+    },
+    {
+        id: 'mecanizaciones-especiales',
+        name: 'MECANIZACIONES ESPECIALES',
+    }
+]
