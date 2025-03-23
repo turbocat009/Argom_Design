ccff9008919ad088a5180e4fd516c002dc7ae4ef
turbocat009 <76442361+turbocat009@users.noreply.github.com>
2025-03-23 17:07:21 +0100

    More Content

---
+++ src/data/rascadores.js
@@ -0,0 +1,208 @@
+export const type = [
+    {
+        id: 'R02',
+        description: 'Rascador de dos alturas para cajera cerrada en poliuretano',
+        SearchValues: 123,
+        content: [
+            {
+                name: 'R02 019047 P93(5X12X2, 80)',
+                stock: 36,
+            },
+            {
+                name: 'R02 023047 P93(6X12X3,00)',
+                stock: 47,
+            },
+            {
+                name: 'R02 023055 P93(6X14X3, 30)',
+                stock: 190,
+            },
+            {
+                name: 'R02 031055 P92(8X14X3, 30)',
+                stock: 187,
+            },
+            {
+                name: 'R02 031055 P93(8X14, 6X3, 80)',
+                stock: 47,
+            },
+            {
+                name: 'R02 039055 P92(10X14X2,00)',
+                stock: 187,
+            },
+            {
+                name: 'R02 039059 CO(10X15X1,00)',
+                stock: 28,
+            },
+            {
+                name: 'R02 039065 P93(10X16, 6X3, 80)',
+                stock: 45,
+            },
+            {
+                name: 'R02 039066 P93(10X17X3, 20)',
+                stock: 'Consultar',
+            },
+            {
+                name: 'R02 039070 P93(10X18X5, 30)',
+                stock: 285,
+            },
+            {
+                name: 'R02 047062 P93(12X16X3, 20)',
+                stock: 89,
+            },
+            {
+                name: 'R02 047070 P93(12X18, 6X3, 80)',
+                stock: 150,
+            },
+            {
+                name: 'R02 051074 P93(13X19, 6X3, 80)',
+                stock: 34,
+            },
+            {
+                name: 'R02 055073 P93(14X18, 6X2, 30)',
+                stock: 95,
+            },
+            {
+                name: 'R02 055078 P93(14X20X3, 80)',
+                stock: 22,
+            },
+            {
+                name: 'R02 057078 P93(14X20, 6X3, 80)',
+                stock: 225,
+            },
+            {
+                name: 'R02 059082 P93(15X21, 6X3, 80)',
+                stock: 84,
+            },
+            {
+                name: 'R02 059106 P92(15X27X5, 30)',
+                stock: 57,
+            },
+            {
+                name: 'R02 062087 P93(16X22, 6X3, 80)',
+                stock: 62,
+            },
+            {
+                name: 'R02 062094 P93(16X24X5, 30)',
+                stock: 30,
+            },
+            {
+                name: 'R02 070094 P93(18X24, 6X3, 80)',
+                stock: 52,
+            },
+            {
+                name: 'R02 070094 P93(18X24, 6X4, 50)',
+                stock: 43,
+            },
+            {
+                name: 'R02 070102 P93(18X26X5, 30)',
+                stock: 19,
+            },
+            {
+                name: 'R02 070118 P92(18X30X5, 30)',
+                stock: 57,
+            },
+            {
+                name: 'R02 074098 P93(19X25X3, 20)',
+                stock: 27,
+            },
+            {
+                name: 'R02 074098 P93(19X25X4, 30)',
+                stock: 47,
+            },
+            {
+                name: 'R02 074110 P92(19X28, 6X5, 30)',
+                stock: 56,
+            },
+            {
+                name: 'R02 075111 P93(19,05X28, 49X4, 75)',
+                stock: 33,
+            },
+            {
+                name: 'R02 078098 P93(20X25X4, 30)',
+                stock: 50,
+            },
+            {
+                name: 'R02 078102 P93(20X26X3, 40)',
+                stock: 195,
+            },
+            {
+                name: 'R02 078104 P93(20X26, 6X3, 40)',
+                stock: 140,
+            },
+        ]
+    },
+    {
+        id: 'R03',
+        description: 'Rascador tres alturas para cajera cerrada en poliuretano',
+        SearchValues: 123,
+    },
+    {
+        id: 'CR2',
+        description: 'Collarín - rascador de dos alturas para cajera cerrada en poliuretano',
+        SearchValues: 123,
+    },
+    {
+        id: 'CR3',
+        description: 'Collarín - rascador de tres alturas para alojamiento cerrado en NBR',
+        SearchValues: 666,
+    },
+    {
+        id: 'PP',
+        description: 'Junta de amortiguación de final de carrera',
+        SearchValues: 123,
+    },
+    {
+        id: 'BWA',
+        description: 'Collarín rascador de dos alturas para neumática',
+        SearchValues: 123,
+    },
+    {
+        id: 'EL',
+        description: 'Collarín - rascador neumático de tres alturas para alojamiento cerrado',
+        SearchValues: 666,
+    },
+    {
+        id: 'EM',
+        description: 'Collarín - rascador neumático de dos alturas para alojamiento cerrado',
+        SearchValues: 123,
+    },
+    {
+        id: 'EF',
+        description: 'Collarín - rascador de tres alturas para cilindros neumático anti - rotación',
+        SearchValues: 123,
+    },
+    {
+        id: 'V6',
+        description: 'Junta de amortiguación de final de carrera',
+        SearchValues: 123,
+    },
+    {
+        id: 'AY',
+        description: 'Collarín - rascador de dos alturas con alojamiento cerrado',
+        SearchValues: 123,
+    },
+    {
+        id: 'R02I',
+        description: 'Collarín - rascador de dos alturas con alojamiento cerrado',
+        SearchValues: 123,
+    },
+    {
+        id: 'CSW',
+        description: 'Rascador de poliuretano para alojamiento cerrado con labio posterior de apoyo',
+        SearchValues: 123,
+    },
+    {
+        id: 'WRS',
+        description: 'Rascador PA cajera cerrada',
+        SearchValues: 123,
+    },
+    {
+        id: 'CR24',
+        description: 'Rascador de dos alturas con cajera inclinada y cerrada',
+        SearchValues: 123,
+    },
+    {
+        id: 'H',
+        description: 'Collarin - rascador para alojamiento cerrado',
+        SearchValues: 123,
+    },
+]; 
